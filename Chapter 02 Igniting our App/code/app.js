3d9ef020e752b2b17f863264b09fe64e02fa96df
JAIJOURA <111792753+JAIJOURA@users.noreply.github.com>
2023-07-21 12:58:03 +0530

    nested element by react.createElement

--- Chapter 02 Igniting our App/code/app.js
+++ Chapter 02 Igniting our App/code/app.js
@@ -1,20 +1,14 @@
-    // const heading= document.createElement('h1')
-    // heading.innerHTML='Hello World'
-    // const root=document.getElementById('root')
-    // root.appendChild(heading)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-    import React from "react"
-    import  ReactDOM  from "react-dom/client"
+ const heading1= React.createElement('h1',{key:'hw'},'Hello World')
+ const heading2= React.createElement('h1',{key:'nr'},'Namaste React')
+ const heading3= React.createElement('h1',{key:'j'},'Jod!!')
 
-    const heading1 = React.createElement('h1', {key:'hw'}, 'Hello World')
-    const heading2 = React.createElement('h1', {key:'nr'}, 'Namaste React')
-    const div = React.createElement('div', {
-        style: {
-            color: 'red'
-        }, className: "nested-div"
-    }, [heading1, heading2])
+const div= React.createElement('div',{style:{
+    color:'red'} , className:"nested-div"},[heading1,heading2,heading3])
 
-    //  console.log(heading) return object 
-    // react create element always give objects
-    const root = ReactDOM.createRoot(document.getElementById('root')) // Create Root element for the React DOM
-    root.render(div)
+//  console.log(heading) return object 
+// react create element always give objects
+const root = ReactDOM.createRoot(document.getElementById('root')) // Create Root element for the React DOM
+root.render(div);
