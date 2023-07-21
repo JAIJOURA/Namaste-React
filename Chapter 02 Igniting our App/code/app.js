b82b32997e871c4413ea396b00da9d60c62a810a
JAIJOURA <111792753+JAIJOURA@users.noreply.github.com>
2023-07-21 13:01:43 +0530

    nested element by jsx

--- Chapter 02 Igniting our App/code/app.js
+++ Chapter 02 Igniting our App/code/app.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
- const heading1= React.createElement('h1',{key:'hw'},'Hello World')
- const heading2= React.createElement('h1',{key:'nr'},'Namaste React')
- const heading3= React.createElement('h1',{key:'j'},'Jod!!')
+//  const heading1= React.createElement('h1',{key:'hw'},'Hello World')
+//  const heading2= React.createElement('h1',{key:'nr'},'Namaste React')
+//  const heading3= React.createElement('h1',{key:'j'},'Jod!!')
 
-const div= React.createElement('div',{style:{
-    color:'red'} , className:"nested-div"},[heading1,heading2,heading3])
+// const div= React.createElement('div',{style:{
+//     color:'red'} , className:"nested-div"},[heading1,heading2,heading3])
+const div = (
+    <div style={{color:'red'}}>
+        <h1>Jai</h1>
+        <h1>Jai</h1>
+        <h1>Jai</h1>
+    </div>
+ )
+
 
 //  console.log(heading) return object 
 // react create element always give objects
