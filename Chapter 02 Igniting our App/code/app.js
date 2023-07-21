3883e2bc33c5a01c0e884ed525cb0d3e9d6a1632
JAIJOURA <111792753+JAIJOURA@users.noreply.github.com>
2023-07-21 21:32:08 +0530

    Chapter 03 Completed

--- Chapter 02 Igniting our App/code/app.js
+++ Chapter 02 Igniting our App/code/app.js
@@ -1,46 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//  const heading1= React.createElement('h1',{key:'hw'},'Hello World')
-//  const heading2= React.createElement('h1',{key:'nr'},'Namaste React')
-//  const heading3= React.createElement('h1',{key:'j'},'Jod!!')
+ const heading1= React.createElement('h1',{key:'hw'},'Hello World')
+ const heading2= React.createElement('h1',{key:'nr'},'Namaste React')
+ const heading3= React.createElement('h1',{key:'j'},'Jod!!')
 
-// const div= React.createElement('div',{style:{
-//     color:'red'} , className:"nested-div"},[heading1,heading2,heading3])
-// const div = (
-//     <div style={{color:'red'}}>
-//         <h1>Jai</h1>
-//         <h1>Jai</h1>
-//         <h1>Jai</h1>
-//     </div>
-//  )
-const heading = ()=>(
-    <div>
-            <h1>Jaiii</h1>
-            <h1>Jaiii</h1>
-            <h1>Jaiii</h1>
-            </div>
-)
-const Divv = ()=>{
-    return (
-        <div className='jai'>
-            {heading()}
-            <h1>JOODDDD!!!</h1>
-            <h1>JOODDDD!!!</h1>
-            <h1>JOODDDD!!!</h1>
-            </div>
-    )
-}
+const div= React.createElement('div',{style:{
+    color:'red'} , className:"nested-div"},[heading1,heading2,heading3])
 
-const Heading =()=>(
-    <div className='flex'> 
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxzdnx_hf5GQJEpX97mkOOzQCTQ0gwTc1TUA&usqp=CAU" alt="logo" />
-<img src="https://t3.ftcdn.net/jpg/05/17/79/88/360_F_517798849_WuXhHTpg2djTbfNf0FQAjzFEoluHpnct.jpg" alt="" />
-     </div>
-)
+    
+
 
 
 //  console.log(heading) return object 
 // react create element always give objects
 const root = ReactDOM.createRoot(document.getElementById('root')) // Create Root element for the React DOM
-root.render(<Heading/>);
+root.render(div);
---
+++ Chapter 03 Laying the Foundation/Theory/Assignment 03.md
@@ -0,0 +1,16 @@
+Q. What is JSX?
+A. JSX is an HTML like syntax used by React that extends ECMASCRIPT so that HTML like syntax can co-exist with JS/React code.
+
+Q. Superpowers of JSX
+A. Using JSX, we can write markup inside js,providing you with a superpower to write logic and markup of a component inside a single jsx  file. JSX is easy to maintain and debug.
+
+Q. Role of type attribute in script tag? What options can I use there?
+A. The type attribute specifies the  type of the script.
+
+Q. {TitleComponent} vs {<TitleComponent/>} vs {<TitleComponent></TitleComponent>} in JSX
+A. {TitleComponent} describes as the js expression or a varible.
+
+{<TitleComponent/>} describes as the functional component which is basically returning some JSX value.
+
+<TitleComponent></TitleComponent> : <TitleComponent /> and <TitleComponent></TitleComponent> are equivalent only when 
+<TitleComponent/> has no child components. The opening and closing tags are created to include the child components.
